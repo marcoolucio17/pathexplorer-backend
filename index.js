aa94d99a3d640e8dcf3e23de13d9fb4798ea4f99
Marco Lucio <marcoalucio15@gmail.com>
2025-04-07 16:18:29 -0600

    connected client to supabase

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to PathExplorer! Your backend is running.');
+// cliente de superbase
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to PathExplorer! Your backend is running.");
+});
+
+app.get("/usuarios", async (req, res) => {
+  const { data, error } = await supabase.from("usuario").select("*");
+
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.status(200).json(data);
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
