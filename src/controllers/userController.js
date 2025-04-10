f62dd4c2481103eff59cddef26dbc0381d35260f
Marco Lucio <marcoalucio15@gmail.com>
2025-04-09 19:40:59 -0600

    Reestructura de los folders así como de implementar la conexión a supabase.

---
+++ src/controllers/userController.js
@@ -0,0 +1,12 @@
+const { fetchUsers } = require('../services/userService');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await fetchUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching users' });
+  }
+};
+
+module.exports = { getUsers };
---
+++ src/services/userService.js
@@ -0,0 +1,15 @@
+const supabase = require('../config/supabaseClient');
+
+const fetchUsers = async () => {
+  const { data, error } = await supabase.from("usuario").select("*");
+  if (error) throw error;
+  return data;
+};
+
+const fetchUserById = async (req, res) => {
+    const { id } = req.params;
+
+    
+}
+
+module.exports = { fetchUsers };
