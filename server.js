ace8e3b622143b91e16f62848712fd9f2488a420
Marco Lucio <marcoalucio15@gmail.com>
2025-04-09 19:45:25 -0600

    eliminar archivo extra de server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://supabase.supabase.co";
-const SUPABASE_ANON_KEY = "supabasekey";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
