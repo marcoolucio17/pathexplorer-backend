791e670d51879d7b3542db1643621daf7570aa4b
Axel Ariel Grande Ruiz <shadow1234f@outlook.com>
2025-05-20 19:30:17 -0600

    Correcciones Merge superagent middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+const authMiddleware = require('../middlewares/verifyHashToken');
+
 // de esto -> router.get('/usuario/:ID', userController.getUserById);
 router.get('/usuario/:ID', authMiddleware, userController.getUserById); // a esto 
 
