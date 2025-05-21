84936c9c9c7cb80ed6a5f3f4cd9c52eb7936ec41
Marco Lucio <marcoalucio15@gmail.com>
2025-05-21 17:44:17 -0600

    added projects, applications, roles, skills

--- src/test/end2end.test.js
+++ src/test/end2end.test.js
@@ -10,9 +10,9 @@ describe("End to end test", () => {
   // prep después del test
   afterAll(async () => {});
 
-  // ## etapa 1.
+  // ## etapa 1. validación de credenciales
   // autenticación
-  test("should return 200 and a token on successful login", async () => {
+  test("should successfully verify a users", async () => {
     const userCredentials = {
       providerid: "antonio.sosa",
       password: "hola123",
@@ -26,9 +26,10 @@ describe("End to end test", () => {
     token = res.body.token;
   });
 
-  // ## etapa 2.
+  // ## etapa 2. proyectos, aplicaciones & notificaciones
+  // todo : agregar ciclos de vida post y delete para sus respectivas pruebas
   // fetch de proyectos
-  test("should return all projects with their roles and status 200", async () => {
+  test("should fetch all projects in the db", async () => {
     const response = await request(app)
       .get("/api/projects")
       .set("Authorization", `Bearer ${token}`);
@@ -66,6 +67,27 @@ describe("End to end test", () => {
     expect(response.statusCode).toBe(200);
   });
 
+  // fetch de todos los roles
+  test("should return all roles with status 200", async () => {
+    const response = await request(app)
+      .get("/api/roles")
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
+
+  // fetch de cierto rol bajo cierto id
+  test("should return a role by id with status 200", async () => {
+    const response = await request(app)
+      .get("/api/roles")
+      .send({ id_rol: "1" })
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
+  });
+
+  // ## etapa 3. skills, perfil
+
   // fetch de las skills técnicas
   test("should return the technical skills with status 200", async () => {
     const response = await request(app)
@@ -88,5 +110,25 @@ describe("End to end test", () => {
       .set("Authorization", `Bearer ${token}`);
 
     expect(response.statusCode).toBe(200);
+  });
+
+  // fetch de todas las metas de cierto usuario
+  test("should return all goals of the user with status 200", async () => {
+    const response = await request(app)
+      .get("/api/goals")
+      .set("Authorization", `Bearer ${token}`)
+      .send({ id_usuario: "1" });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
+
+  // fetch de cierta meta de cierto usuario
+  test("should return a goal by id with status 200", async () => {
+    const response = await request(app)
+      .get("/api/goals")
+      .set("Authorization", `Bearer ${token}`)
+      .send({ id_goal: "2" });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
   });
 });
