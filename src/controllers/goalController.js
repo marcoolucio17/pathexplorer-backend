19c8411b1edfc00ce635accd4c27d30bb75d9a0a
JUANECL314 <a01198418@tec.mx>
2025-05-31 16:59:39 -0600

    Avances

--- src/controllers/goalController.js
+++ src/controllers/goalController.js
@@ -1,151 +1,148 @@
-const { 
-    fetchGoal,
-    fetchGoalById,
-    fetchCreateGoal,
-    fetchUpdateGoal,
-    fetchDeleteGoal} = require('../services/goalService');
+const {
+  fetchGoal,
+  fetchGoalById,
+  fetchCreateGoal,
+  fetchUpdateGoal,
+  fetchDeleteGoal,
+} = require("../services/goalService");
 
 //Función para obtener todas las metas de un usuario
 const getGoals = async (req, res) => {
-    try {
-        const { id_usuario = null, id_goal = null} = req.body || {};
-        //Consulta para todas las metas de un usuario
-        if (id_usuario) {
-            getGoalsUser(req, res);
-        } else if (id_goal) {
-        //Consulta para una meta por id
-            getGoalById(req, res);
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching goals' });
+  try {
+    const { id_usuario = null, id_goal = null } = req.query || {};
+    //Consulta para todas las metas de un usuario
+    if (id_usuario) {
+      console.log(id_usuario);
+      getGoalsUser(req, res);
+    } else if (id_goal) {
+      //Consulta para una meta por id
+      getGoalById(req, res);
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching goals" });
+  }
+};
 
 //Función para obtener una meta por id
 // Revisado
-const getGoalsUser = async (req, res) => { 
-    try {
-        const { id_usuario = null} = req.body || {};
-        const goals = await fetchGoal(id_usuario);
-        if (!goals) {
-            res.status(404).json({ error: 'No goals found for this user' });
-        }
-        res.status(200).json(goals);
-    } catch (error) { 
-        res.status(500).json({ error: 'Error fetching goals' });
+const getGoalsUser = async (req, res) => {
+  try {
+    const { id_usuario = null } = req.query || {};
+    const goals = await fetchGoal(id_usuario);
+    if (!goals) {
+      res.status(404).json({ error: "No goals found for this user" });
     }
-}
+    res.status(200).json(goals);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching goals" });
+  }
+};
 
 //Revisado
-const getGoalById = async (req, res) => { 
-    try {
-        const { id_goal = null } = req.body || {};
-        const goal = await fetchGoalById(id_goal);
-        if (!goal) {
-            res.status(404).json({ error: 'Goal not found' });
-        }
-        res.status(200).json(goal);
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching goal' });
+const getGoalById = async (req, res) => {
+  try {
+    const { id_goal = null } = req.query || {};
+    const goal = await fetchGoalById(id_goal);
+    if (!goal) {
+      res.status(404).json({ error: "Goal not found" });
     }
-}
+    res.status(200).json(goal);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching goal" });
+  }
+};
 
 //Función para crear una meta
 const createGoal = async (req, res) => {
-    try {
-        const { goal = null } = req.body.informacion || {};
-        if (goal) {
-            createNewGoal(req, res);
-        } else {
-            res.status(400).json({ error: 'Goal is required' });
-        }
-
-    } catch (error) {
-        res.status(500).json({ error: 'Error creating goal' });
+  try {
+    const { goal = null } = req.body.informacion || {};
+    if (goal) {
+      createNewGoal(req, res);
+    } else {
+      res.status(400).json({ error: "Goal is required" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: "Error creating goal" });
+  }
+};
 
 const createNewGoal = async (req, res) => {
-    try { 
-        const { goal = null } = req.body.informacion || {};
-        
-        const result = await fetchCreateGoal(goal);
-        if (!result) {
-            res.status(400).json({ error: 'Error creating goal' });    
-        }
-        res.status(201).json({ message: 'Goal created successfully' });
-    } catch (error) { 
-        res.status(500).json({ error: 'Error creating goal' });
-    }
-}
+  try {
+    const { goal = null } = req.body.informacion || {};
 
+    const result = await fetchCreateGoal(goal);
+    if (!result) {
+      res.status(400).json({ error: "Error creating goal" });
+    }
+    res.status(201).json({ message: "Goal created successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error creating goal" });
+  }
+};
 
 //Función para actualizar una meta por id
-const updateGoal = async (req, res) => {   
-    try {
-        const { idmeta = null } = req.body || {};
-        const { goal = null} = req.body.informacion || {};
-        
-        if (idmeta && goal) {
-            updatingAGoalUser(req, res);
-        } else {
-            res.status(400).json({ error: 'User ID and goal are required' });
-        }
-       
-    } catch (error) {
-        res.status(500).json({ error: 'Error updating goal' });
-    }
- 
-}
+const updateGoal = async (req, res) => {
+  try {
+    const { idmeta = null } = req.body || {};
+    const { goal = null } = req.body.informacion || {};
 
-const updatingAGoalUser = async(req, res) => {
-    try {
-        const { idmeta = null } = req.body || {};
-        const { goal = null } = req.body.informacion || {};
-        console.log(idmeta, goal);
-        const result = await fetchUpdateGoal(idmeta, goal);
-        if (result) {
-            res.status(200).json({ message: 'Goal updated successfully' });
-        } else {
-            res.status(404).json({ error: 'Goal not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Error updating goal' });
+    if (idmeta && goal) {
+      updatingAGoalUser(req, res);
+    } else {
+      res.status(400).json({ error: "User ID and goal are required" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: "Error updating goal" });
+  }
+};
+
+const updatingAGoalUser = async (req, res) => {
+  try {
+    const { idmeta = null } = req.body || {};
+    const { goal = null } = req.body.informacion || {};
+    console.log(idmeta, goal);
+    const result = await fetchUpdateGoal(idmeta, goal);
+    if (result) {
+      res.status(200).json({ message: "Goal updated successfully" });
+    } else {
+      res.status(404).json({ error: "Goal not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error updating goal" });
+  }
+};
 
 //Función para eliminar una meta por id
-const deleteGoal = async (req, res) => { 
-    try {
-        const { id_goal = null } = req.body || {};
-        if (id_goal) {
-            deleteGoalById(req, res);
-        } else {
-            res.status(400).json({ error: 'Goal ID is required' });
-        }
-    
-    } catch (error) {
-        res.status(500).json({ error: 'Error deleting goal' });
+const deleteGoal = async (req, res) => {
+  try {
+    const { id_goal = null } = req.body || {};
+    if (id_goal) {
+      deleteGoalById(req, res);
+    } else {
+      res.status(400).json({ error: "Goal ID is required" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting goal" });
+  }
+};
 
-const deleteGoalById = async (req, res) => { 
-    try {
-        const { id_goal = null } = req.body || {};
-        const result = await fetchDeleteGoal(id_goal);
-        if (result) {
-            res.status(200).json({ message: 'Goal deleted successfully' });
-        } else {
-            res.status(404).json({ error: 'Goal not found' });
-        }
-    } catch (error) { 
-        res.status(500).json({ error: 'Error deleting goal' });
+const deleteGoalById = async (req, res) => {
+  try {
+    const { id_goal = null } = req.body || {};
+    const result = await fetchDeleteGoal(id_goal);
+    if (result) {
+      res.status(200).json({ message: "Goal deleted successfully" });
+    } else {
+      res.status(404).json({ error: "Goal not found" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting goal" });
+  }
+};
 
-module.exports = { 
-    getGoals, 
-    createGoal,
-    updateGoal,
-    deleteGoal
+module.exports = {
+  getGoals,
+  createGoal,
+  updateGoal,
+  deleteGoal,
 };
--- src/services/projectService.js
+++ src/services/projectService.js
@@ -48,22 +48,6 @@ const fetchProjects = async () => {
   return dataProyectos;
 };
 
-const fetchMyProjects = async (id_usuario) => {
-  const { data, error } = await supabase
-    .from("proyecto")
-    .select(textToObtainInfoProject)
-    .eq("idusuario", id_usuario);
-  if (error) {
-    console.log("error", error);
-    throw new ApiError(
-      error.status || 400,
-      error.message || "There is an error fetching the projects."
-    );
-  }
-
-  return data;
-};
-
 /**
  */
 
@@ -425,7 +409,6 @@ const getRFPSignedUrl = async (projectId) => {
 
 module.exports = {
   fetchProjects,
-  fetchMyProjects,
   fetchProjectById,
   fetchProjectsByName,
   fetchCreateProject,
